87aa84d8d0321bd5081bb40c7b292fc623fe9896
David Rodrigues <55930667+davidjmrodrigues@users.noreply.github.com>
2026-06-17 12:36:10 +0100

    feat(application): added GetCountryList handler and query

---
+++ NextHoliday.Application/Countries/Queries/GetAllCountries/GetAllCountriesHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NextHoliday.Infrastructure.Persistence;
+
+namespace NextHoliday.Application.Countries.Queries.GetAllCountries
+{
+    public class GetAllCountriesHandler : IRequestHandler<GetAllCountriesQuery, List<CountryDto>?>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetAllCountriesHandler(ApplicationDbContext context) => _context = context;
+
+        public async Task<List<CountryDto>?> Handle(GetAllCountriesQuery request, CancellationToken cancellationToken)
+        {
+            var countries = await _context.Countries.ToListAsync(cancellationToken);
+
+            return countries.Select(c => new CountryDto(c.Code, c.Name, c.Continent)).ToList();
+        }
+    }
+}
---
+++ NextHoliday.Application/Countries/Queries/GetAllCountries/GetAllCountriesQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using NextHoliday.Domain.Enums;
+
+namespace NextHoliday.Application.Countries.Queries.GetAllCountries
+{
+    public record GetAllCountriesQuery() : IRequest<List<CountryDto>>;
+    public record CountryDto(
+        string Code,
+        string Name,
+        Continent Continent
+    );
+}
