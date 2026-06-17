dc6a8affe3a266f5cf714ece4326ccf9f63e2926
David Rodrigues <55930667+davidjmrodrigues@users.noreply.github.com>
2026-06-17 12:40:37 +0100

    feat(api): Added GetAllCountries endpoint.

---
+++ NextHoliday.API/Endpoints/CountryEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using NextHoliday.Application.Countries.Queries.GetAllCountries;
+
+namespace NextHoliday.API.Endpoints
+{
+    public class CountryEndpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("countries").WithTags("Countries");
+
+            group.MapGet("get", async (IMediator mediator) =>
+            {
+                var query = new GetAllCountriesQuery();
+                var result = await mediator.Send(query);
+                return Results.Ok(result);
+            })
+            .WithName("GetAllCountries");
+        }
+    }
+}
