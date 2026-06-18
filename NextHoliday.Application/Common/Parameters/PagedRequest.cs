cabe1a51769f184137a96040bcc092e9c6d9a849
David Rodrigues <55930667+davidjmrodrigues@users.noreply.github.com>
2026-06-18 21:49:49 +0100

    feat: added pagination params

---
+++ NextHoliday.Application/Common/Parameters/PagedRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace NextHoliday.Application.Common.Parameters
+{
+    public record PagedRequest
+    {
+        public string? Search { get; init; } = null;
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 10;
+    }
+}
--- NextHoliday.Application/Entities/Countries/Queries/GetAllCountries/GetAllCountriesQuery.cs
+++ NextHoliday.Application/Entities/Countries/Queries/GetAllCountries/GetAllCountriesQuery.cs
@@ -1,13 +1,12 @@
 ﻿using MediatR;
+using NextHoliday.Application.Common.Parameters;
 
 namespace NextHoliday.Application.Entities.Countries.Queries.GetAllCountries
 {
     public record GetAllCountriesQuery(
-        String? Search = null,
-        String? Continent = null,
-        int Page = 1,
-        int PageSize = 50
-        ) : IRequest<IEnumerable<CountryDto>>;
+        String? Continent = null
+        ) : PagedRequest, IRequest<IEnumerable<CountryDto>>;
+
     public record CountryDto(
         string Code,
         string Name,
