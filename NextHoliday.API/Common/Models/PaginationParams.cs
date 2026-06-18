b2cf02b70b594879434c46c92522b9a884a57af7
David Rodrigues <55930667+davidjmrodrigues@users.noreply.github.com>
2026-06-18 21:25:15 +0100

    feat: created record PaginationParams

---
+++ NextHoliday.API/Common/Models/PaginationParams.cs
@@ -0,0 +1,9 @@
+﻿namespace NextHoliday.API.Common.Models
+{
+    public record struct PaginationParams
+    (
+        string? Search = null,
+        int Page = 1,
+        int PageSize = 50
+    );
+}
