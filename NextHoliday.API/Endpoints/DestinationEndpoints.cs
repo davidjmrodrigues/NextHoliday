8541e0fce9fba6d709864c95f8dde480fdb47c1a
David Rodrigues <55930667+davidjmrodrigues@users.noreply.github.com>
2026-06-19 15:11:03 +0100

    feat: create endpoint GET destination by id

--- NextHoliday.API/Endpoints/DestinationEndpoints.cs
+++ NextHoliday.API/Endpoints/DestinationEndpoints.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NextHoliday.Application.Features.Destinations.Commands.CreateDestination;
 using NextHoliday.Application.Features.Destinations.Queries.GetBestDestination;
+using NextHoliday.Application.Features.Destinations.Queries.GetDestinationbyId;
 using NextHoliday.Domain.Enums;
 
 namespace NextHoliday.API.Endpoints
@@ -12,6 +13,17 @@ namespace NextHoliday.API.Endpoints
         {
             var group = app.MapGroup("destinations").WithTags("Destinations");
 
+            group.MapGet("{id:guid}", async (Guid id, IMediator mediator) =>
+            {
+                var query = new GetDestinationByIdQuery(id);
+                var result = await mediator.Send(query);
+                return Results.Ok(result);
+            })
+            .WithName("DestinationById")
+            .Produces<Application.Features.Destinations.Queries.GetDestinationbyId.DestinationDto>(StatusCodes.Status200OK)
+            .Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
+            .Produces<ProblemDetails>(StatusCodes.Status404NotFound);
+
             // GET BEST
             group.MapGet("best", async (Continent? continent, int? month, IMediator mediator) =>
             {
@@ -20,7 +32,7 @@ namespace NextHoliday.API.Endpoints
                 return Results.Ok(result);
             })
             .WithName("GetBestDestination")
-            .Produces<DestinationDto>(StatusCodes.Status200OK)
+            .Produces<Application.Features.Destinations.Queries.GetBestDestination.DestinationDto>(StatusCodes.Status200OK)
             .Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
             .Produces<ProblemDetails>(StatusCodes.Status404NotFound);
 
---
+++ NextHoliday.Application/Features/Destinations/Queries/GetDestinationbyId/GetDestinationByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using NextHoliday.Domain.Entities.History;
+
+namespace NextHoliday.Application.Features.Destinations.Queries.GetDestinationbyId
+{
+    public record GetDestinationByIdQuery(Guid Id) : IRequest<DestinationDto>;
+
+    public record DestinationDto(
+        Guid Id,
+        string City,
+        string CountryCode,
+        string CountryName,
+        string Description,
+        bool IsActive,
+        IEnumerable<ClimateHistoryDto> ClimateHistories,
+        IEnumerable<PriceHistoryDto> PriceHistories
+    );
+
+    public record ClimateHistoryDto(int Month, double AverageTemperature, double RainProbability, string WeatherCondition);
+    public record PriceHistoryDto(int Month, decimal EstimatedFlightPrice, decimal EstimatedHotelPricePerNight);
+}
