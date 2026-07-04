24d0d8c854ac6cbee8fcdf7c5d1541b8ec7c561a
David Rodrigues <55930667+davidjmrodrigues@users.noreply.github.com>
2026-07-04 23:01:37 +0100

    feat: added background service to run weather service

---
+++ NextHoliday.Infrastructure/Services/WeatherSyncBackgroundService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NextHoliday.Infrastructure.Services
+{
+    public class WeatherSyncBackgroundService(IServiceProvider serviceProvider) : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider = serviceProvider;
+        private readonly TimeSpan _period = TimeSpan.FromHours(12);
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using PeriodicTimer timer = new(_period);
+
+            while (await timer.WaitForNextTickAsync(stoppingToken) && !stoppingToken.IsCancellationRequested)
+            {
+                await TriggerSyncAsync();
+            }
+        }
+
+        private async Task TriggerSyncAsync()
+        {
+            Console.WriteLine($"[Background Job] Initiating automatic weather sync: {DateTime.UtcNow}");
+
+            using var scope = _serviceProvider.CreateScope();
+            var syncService = scope.ServiceProvider.GetRequiredService<WeatherSyncService>();
+
+            await syncService.SyncWeatherForDestinationsAsync();
+
+            Console.WriteLine($"[Background Job] Weather sync completed: {DateTime.UtcNow}");
+        }
+    }
+}
