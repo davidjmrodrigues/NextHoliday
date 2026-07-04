72e1a23626dc7874166f1f7890faec498452edde
David Rodrigues <55930667+davidjmrodrigues@users.noreply.github.com>
2026-07-04 23:09:50 +0100

    feat: created admin endpoint group and added manual weather sync endpoint

---
+++ NextHoliday.API/Endpoints/AdminEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using NextHoliday.Infrastructure.Services;
+
+namespace NextHoliday.API.Endpoints
+{
+    public class AdminEndpoints : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("admin");
+
+            // WEATHER
+            var weatherGroup = group.MapGroup("weather");
+
+            weatherGroup.MapPost("sync", async (WeatherSyncService syncService) =>
+            {
+                Console.WriteLine("[Manual Sync] Weather data sync initiated via API...");
+
+                await syncService.SyncWeatherForDestinationsAsync();
+
+                return Results.Ok();
+            })
+            .WithName("ForceWeatherSync");
+        }
+    }
+}
